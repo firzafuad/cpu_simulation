9aabe2f60327c5aa6810a37fa4329a3d65d78688
Fuadi Achmad Firza <achmad_firza.fuadi@etu.sorbonne-universite.fr>
2025-04-02 20:21:38 +0000

    Upload New File

---
+++ parser.h
@@ -0,0 +1,19 @@
+#include "segment.h"
+typedef struct {
+	char* mnemonic ; // Instruction mnemonic (ou nom de variable pour .DATA)
+	char* operand1 ; // Premier operande (ou type pour .DATA)
+	char* operand2 ; // Second operande (ou initialisation pour .DATA)
+} Instruction ;
+
+typedef struct {
+	Instruction ** data_instructions ; // Tableau d’instructions .DATA
+	int data_count ; // Nombre d’instructions .DATA
+	Instruction ** code_instructions ; // Tableau d’instructions .CODE
+	int code_count ; // Nombre d’instructions .CODE
+	HashMap * labels ; // labels -> indices dans code_instructions
+	HashMap * memory_locations ; // noms de variables -> adresse memoire
+} ParserResult ;
+
+Instruction *parse_data_instruction(const char *line, HashMap* memory_locations);
+Instruction *parse_code_instruction(const char *line, HashMap* labels, int code_count);
+ParserResult *parse(const char *filename);
